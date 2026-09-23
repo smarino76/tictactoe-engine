63f2fb5aef2308ea2775b616e91c9316b3b0a524
Santiago <santiago.mmarino@gmail.com>
2026-09-23 22:37:11 +0200

    Implement test for get_random_action
    Add a test for the get_random_action function.

---
+++ CI_CD/CI/test_actions_extract.py
@@ -0,0 +1,7 @@
+from agents.helpers.actions_extract import get_random_action, get_available_actions
+
+
+def test_get_random_action():
+  state = [1,1,1,1,1,1,1,None,1]
+  result = get_random_action(state)
+  assert restul == 7

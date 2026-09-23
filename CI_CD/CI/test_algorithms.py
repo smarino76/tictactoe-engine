a240aaa477e57ef118bed9c3f985ee0304d58260
Santiago Marino <santiago.mmarino@gmail.com>
2026-09-23 23:21:05 +0200

    test: fix critical move propagation expectation

--- CI_CD/CI/test_algorithms.py
+++ CI_CD/CI/test_algorithms.py
@@ -1,11 +1,15 @@
 import numpy as np
-from agents.algorithm.reward_functions import algorithm_critical_move_propagation, algorithm_simple_inverse_propagation
+from agents.algorithm.reward_functions import algorithm_critical_move_propagation
+
 
 def test_algorithm_critical_move_propagation():
-  targets_0 = [.6,.23,.3,.55,.14]
-  targets_1 = [0,0,0,0]
-  
-  algorithm_critical_move_propagation(0,targets_0)
-  algorithm_critical_move_propagation(1,targets_1)
-  
-  assert targets_0[3] == 0 and targets_1[2] == 1 and targets_1[3] == np.power((1 * 0.9),2)
+    targets_draw = [0.6, 0.23, 0.3, 0.55, 0.14]
+    targets_win = [0, 0, 0, 0]
+
+    algorithm_critical_move_propagation(0, targets_draw)
+    algorithm_critical_move_propagation(1, targets_win)
+
+    assert targets_draw[3] == 0
+    assert targets_win[2] == 1
+    assert targets_win[3] == np.power(1 * 0.9, 1)
+    assert targets_win[1] == np.power(targets_win[3] * 0.9, 2)

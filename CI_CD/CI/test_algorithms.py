c1332cc29ba71330fdd1b4bc016c630f2e4d385c
Santiago Marino <santiago.mmarino@gmail.com>
2026-09-23 23:16:56 +0200

    test: expand reward propagation coverage

--- CI_CD/CI/test_algorithms.py
+++ CI_CD/CI/test_algorithms.py
@@ -1,7 +1,11 @@
-
+import numpy as np
 from agents.algorithm.reward_functions import algorithm_critical_move_propagation, algorithm_simple_inverse_propagation
 
 def test_algorithm_critical_move_propagation():
-  targets = [.6,.23,.3,.55,.14]
-  algorithm_critical_move_propagation(0,targets)
-  assert targets[3] == 0
+  targets_0 = [.6,.23,.3,.55,.14]
+  targets_1 = [0,0,0,0]
+  
+  algorithm_critical_move_propagation(0,targets_0)
+  algorithm_critical_move_propagation(1,targets_1)
+  
+  assert targets_0[3] == 0 and targets_1[2] == 1 and targets_1[3] == np.power((1 * 0.9),2)

8ff540969424a77975e2f056be12b2f6410c535d
Santiago <santiago.mmarino@gmail.com>
2026-09-23 22:38:27 +0200

    Fix typo in test assertion for get_random_action

--- CI_CD/CI/test_actions_extract.py
+++ CI_CD/CI/test_actions_extract.py
@@ -4,4 +4,4 @@ from agents.helpers.actions_extract import get_random_action, get_available_acti
 def test_get_random_action():
   state = [1,1,1,1,1,1,1,None,1]
   result = get_random_action(state)
-  assert restul == 7
+  assert result == 7
